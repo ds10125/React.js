234b7bd88dd71e8a043d8e4db9bf2e42261ffc0e
ds10125 <ds11705@hotmail.com>
2017-04-06 21:57:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'; //引用函式庫不需指定路徑
 
 import SearchBar from './components/search_bar'; //自己寫的js要指定路徑
 
-const API_KEY = 'AIzaSyB7CB8xxY99yGbD83Pga9bpWyQC0BrZJS4';
+const API_KEY = 'AIzaSyB7CB8xxY99yGbD83Pga9bpW*';
 // Create a new Component to produce HTML using JSX
 
 const App =  () => {
