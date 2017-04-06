17995e6ab55ee7c980c86c857f884ff8a509726f
ds10125 <ds11705@hotmail.com>
2017-04-06 21:56:22 +0800

    Update search_bar.js

--- search_bar.js
+++ search_bar.js
@@ -1,7 +1,24 @@
-inport React from 'react'
+import React, { Component } from 'react'
 
-const SerchBar = () =>{
-  return <input /> //React.createElement
+class SerchBar extends Component {
+
+constructor(props){
+  super(props);
+  this.state = {term: ''}; //只有在constructor 可以用this.state賦值
+}
+//其他地方都要用this.setState  賦值
+ //JSX取變數用大括號{}
+  render (){
+      return(
+      <div>
+      <input
+       value = {this.state.term}
+       onChange={(event) =>this.setState({term: event.target.value})} />
+
+    </div>)
+  }
+
+
 }
 
 export default SerchBar
