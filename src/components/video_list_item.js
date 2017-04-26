68b1c5d3318b50e75965b657b238e1743cc79df4
ds10125 <ds11705@hotmail.com>
2017-04-26 22:37:21 +0800

    Update video_list_item.js

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const VideoListItem = ({video}) =>{
-//  const video = props.video
+const VideoListItem = ({video, onVideoSelect}) =>{
+  /* 等同
+  const video = props.video;
+  const onVideoSelect = props.onVideoSelect
+ */
  const imageUrl =  video.snippet.thumbnails.default.url;//from youtube-api
 console.log(video);
   return (
-    <li className="list-group-item">
+    <li onClick = {() => onVideoSelect(video)}
+    className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl}/>
