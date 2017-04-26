cd1b6c7ed17c6de6abe818dd90a8e68f344545e3
ds10125 <ds11705@hotmail.com>
2017-04-26 22:37:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom'; //引用函式庫不需指定路徑
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'; //自己寫的js要指定路徑
-
-const API_KEY = 'AIzaSyB7CB8xxY99yGbD83Pga9bpW*';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBgx_X3l6St2nmJFNhqS6aftJBqfYvVgwc';
 // Create a new Component to produce HTML using JSX
 
-const App =  () => {
-  return (
-    <div><SearchBar /> </div>
-  );
+
+class App extends Component   {
+  constructor(props){
+    super(props);
+    this.state = {
+      videos:[],
+      selectedVideo:null
+    };
+    //function(data) = (data) =>
+    YTSearch({key:API_KEY , term:'CPU'}, (videos) =>{
+      this.setState({
+          videos:videos,
+          selectedVideo:videos[0]
+      }); // state變數名稱跟值依樣, video:video可簡化為 video
+    })
+  }
+  render(){
+    return (
+    <div>
+      <SearchBar />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+      onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos = {this.state.videos} />
+    </div>
+    );
+ }
 }
 // Take the Component to HTML
 ReactDOM.render(<App />,document.querySelector('.container'));
